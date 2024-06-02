e0ae28a62acf151030bbc99e2ac826625a924928
JungDeunGyul <ask7384@gmail.com>
2024-06-03 05:33:23 +0900

    style: Changed style for loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,13 +54,11 @@ function App() {
   }, []);
 
   return (
-    <main
-      className={`w-80 h-50 max-h-[96] bg-gradient-to-b from-gray-700 via-gray-900 to-gray-800`}
-    >
+    <main className="w-80 h-50 bg-gradient-to-b from-gray-700 via-gray-900 to-gray-800">
       {loading ? (
-        <div className="flex flex-col items-center justify-center h-full bg-gradient-to-b from-gray-700 via-gray-500 to-gray-400">
-          <div className="w-32 h-32 border-t-8 border-red-300 rounded-full animate-spin border-opacity-30"></div>
-          <div>Loading...</div>
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="w-32 h-32 border-t-8 border-red-300 rounded-full animate-spin"></div>
+          <div className="mt-4 text-white">Loading...</div>
         </div>
       ) : userData ? (
         <div className="bg-gradient-to-b from-gray-700 via-gray-900 to-gray-800">
